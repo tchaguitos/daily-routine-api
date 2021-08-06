649efa6fa838009c39bbe2dea14dddd2eb0b7890
Thiago Brasil <tchaguitos@gmail.com>
2021-08-06 20:20:11 -0300

    :tada: commit inicial

---
+++ api/main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/check")
+def check_status():
+    return {"message": "service running"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
